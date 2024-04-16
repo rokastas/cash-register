f642d96c9b443fd999d6f63009dcebf8cf336331
Rokas Stasiulis <rokastas@gmail.com>
2024-04-16 15:20:16 +0200

    consistent syntax - adjusted quotes

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,12 +1,12 @@
 require_relative '../src/product'
 
 RSpec.describe Product do
-  let(:product) { Product.new("GR1", "Green Tea", 3.11) }
+  let(:product) { Product.new('GR1', 'Green Tea', 3.11) }
 
-  describe "#initialize" do
-    it "sets the code, name, and price attributes" do
-      expect(product.code).to eq("GR1")
-      expect(product.name).to eq("Green Tea")
+  describe '#initialize' do
+    it 'sets the code, name, and price attributes' do
+      expect(product.code).to eq('GR1')
+      expect(product.name).to eq('Green Tea')
       expect(product.price).to eq(3.11)
     end
   end
--- src/user_interface.rb
+++ src/user_interface.rb
@@ -1,52 +1,52 @@
 class User_interface
   def display_welcome_message
-    puts "-------------------------------------------"
-    puts "----- Welcome to the Checkout System -----"
-    puts "-------------------------------------------"
-    puts ""
+    puts '-------------------------------------------'
+    puts '----- Welcome to the Checkout System -----'
+    puts '-------------------------------------------'
+    puts ''
   end
 
   def ask_for_action
-    puts "Please select an action:"
-    puts "1. Add products to cart"
-    puts "2. Checkout cart"
-    puts "3. Exit program"
-    print "> "
+    puts 'Please select an action:'
+    puts '1. Add products to cart'
+    puts '2. Checkout cart'
+    puts '3. Exit program'
+    print '> '
     gets.chomp
   end
 
   def display_products(products)
-    puts ""
-    puts "Select a product:"
+    puts ''
+    puts 'Select a product:'
     products.each_with_index do |product, index|
       puts "#{index + 1}. #{product.name} - #{product.price} €"
     end
   end
 
   def ask_for_product_index
-    puts "Enter the number of the product you want to add: "
-    print "> "
+    puts 'Enter the number of the product you want to add: '
+    print '> '
     gets.chomp
   end
 
   def ask_for_product_quantity
-    puts "Enter the quantity of the product: "
-    print "> "
+    puts 'Enter the quantity of the product: '
+    print '> '
     gets.chomp
   end
 
   def display_cart(cart)
-    puts ""
-    puts "--------------- Your Cart ---------------"
+    puts ''
+    puts '--------------- Your Cart ---------------'
     if cart.empty?
-      puts "Your cart is empty."
+      puts 'Your cart is empty.'
     else
       cart.each do |product, quantity|
         puts "#{product.name} (#{quantity}x) - ??? €"
       end
     end
-    puts "-----------------------------------------"
-    puts "Total: ??? €"
-    puts ""
+    puts '-----------------------------------------'
+    puts 'Total: ??? €'
+    puts ''
   end
 end
